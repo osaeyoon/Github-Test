4b43e9474994b240e394ee398572607bb858f62e
USER <USER@DESKTOP-657EF2G>
2019-11-09 18:30:21 +0900

    first commit : midterm
    my midterm answer to test github.

---
+++ midterm/src/Q3.java
@@ -0,0 +1,16 @@
+
+
+public class Q3 {
+	public static int getCRT(int p1, int p2, int r1, int r2) {
+		int result = -1;
+		for (int i = 0; i < p1 * p2; i++) {
+			// fill here!
+			if (r1 == i % p1 && r2 == i % p2)
+				result = i;		
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		System.out.println(getCRT(5, 7, 3, 4)); // 18
+	}
+}
---
+++ midterm/src/Q5.java
@@ -0,0 +1,70 @@
+
+
+class Currency {
+	private double dollars;
+	private double won;
+ 	private double yen;
+	
+	public Currency(double initial_amount) {
+		// initial_amount is given in won 
+		// fill here! 
+		if (initial_amount > 0) {
+			won = initial_amount;
+			dollars = initial_amount * 0.001;
+			yen = initial_amount * 0.1;
+		}
+		else {
+			won = 0; dollars = 0; yen = 0;
+		}
+	}
+	public Currency add(Currency other) {
+		// fill here!
+		double new_amount;
+		new_amount = won + other.getWon();
+		Currency result = new Currency(new_amount);
+		return result;
+	}
+	public double getDollars() {
+		// fill here!
+		return dollars;
+	}
+	public double getWon() {
+		// fill here!
+		return won;
+	}
+	public double getYen() {
+		// fill here!
+		return yen;
+	}
+}
+
+class BankAccount {
+    private Currency balance;
+
+    public BankAccount(Currency initial_amount) {
+    	// fill here!
+    	balance = initial_amount;
+    }
+
+    public Currency getBalance() {
+    	// fill here!
+    	return balance;
+    }
+
+    public boolean deposit(Currency money) {
+    	// fill here!
+    	boolean result = false;
+    	balance = balance.add(money);
+    	result = true;
+    	return result;
+    }
+}
+
+public class Q5 {
+  public static void main(String[] args) {
+    Currency money = new Currency(100); 
+    BankAccount account = new BankAccount(money);
+    account.deposit(money);
+    System.out.println("balance = " + account.getBalance().getDollars()); // balance = 0.2
+  }
+}
